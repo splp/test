a7314f0f0e7ea5170fdeea782cc7096a2a5701c5
mac <mac@macdeMacBook-Air.local>
2015-10-29 22:06:21 +0800

    new file:   scancode.xcodeproj/project.pbxproj 	new file:   scancode.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   scancode.xcodeproj/project.xcworkspace/xcuserdata/mac.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   scancode.xcodeproj/xcuserdata/mac.xcuserdatad/xcschemes/scancode.xcscheme 	new file:   scancode.xcodeproj/xcuserdata/mac.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   scancode/AppDelegate.swift 	new file:   scancode/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   scancode/Base.lproj/LaunchScreen.storyboard 	new file:   scancode/Base.lproj/Main.storyboard 	new file:   scancode/Info.plist 	new file:   scancode/ViewController.swift 	new file:   scancodeTests/Info.plist 	new file:   scancodeTests/scancodeTests.swift 	new file:   scancodeUITests/Info.plist 	new file:   scancodeUITests/scancodeUITests.swift

---
+++ scancode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scancode
+//
+//  Created by mac on 15/8/30.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
